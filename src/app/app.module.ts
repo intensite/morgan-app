d2af455d2a4da83cadaa775b81a060b572feef0c
Stephen Remillard <sremillard@pciauto.com>
2020-03-22 01:47:18 -0400

    Removed CDN for bootstrap and fontawsome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import * as _ from 'lodash';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +27,8 @@ import { GuidanceComponent } from './guidance/guidance.component';
     AppRoutingModule,
     WebBluetoothModule.forRoot({
       enableTracing: false // or false, this will enable logs in the browser's console
-    })
+    }),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
