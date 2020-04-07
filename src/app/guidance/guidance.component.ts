eefef0626b992c6f2bc84d905454b1b0b204c6c5
Stephen Remillard <sremillard@pciauto.com>
2020-04-07 00:44:49 -0400

    Beginig of the guidance form setup

--- src/app/guidance/guidance.component.ts
+++ src/app/guidance/guidance.component.ts
@@ -13,18 +13,73 @@ import * as _ from 'lodash';
 })
 export class GuidanceComponent implements OnInit {
 
+    guidanceForm = this.fb.group({
+        GUIDING_TYPE: [1, {updateOn: "blur"}],
+        ROLL_CONTROL_ENABLED: [1, {updateOn: "blur"}],
+        ROLL_CONTROL_TYPE: ["", {updateOn: "blur"}],
+
+        SERVO_1_AXIS: ["", {updateOn: "blur"}],
+        SERVO_2_AXIS: ["", {updateOn: "blur"}],
+        SERVO_3_AXIS: ["", {updateOn: "blur"}],
+        SERVO_4_AXIS: ["", {updateOn: "blur"}],
+
+        SERVO_1_OFFSET: ["", {updateOn: "blur"}],
+        SERVO_2_OFFSET: ["", {updateOn: "blur"}],
+        SERVO_3_OFFSET: ["", {updateOn: "blur"}],
+        SERVO_4_OFFSET: ["", {updateOn: "blur"}],
+        SERVO_1_ORIENTATION: ["", {updateOn: "blur"}],
+        SERVO_2_ORIENTATION: ["", {updateOn: "blur"}],
+        SERVO_3_ORIENTATION: ["", {updateOn: "blur"}],
+        SERVO_4_ORIENTATION: ["", {updateOn: "blur"}],
+        MAX_FINS_TRAVEL: ["", {updateOn: "blur"}],
+
+        PID_PITCH_Kp: ["", {updateOn: "blur"}],
+        PID_PITCH_Ki: ["", {updateOn: "blur"}],
+        PID_PITCH_Kd: ["", {updateOn: "blur"}],
+
+        PID_YAW_Kp: ["", {updateOn: "blur"}],
+        PID_YAW_Ki: ["", {updateOn: "blur"}],
+        PID_YAW_Kd: ["", {updateOn: "blur"}],
+        
+        PID_ROLL_Kp: ["", {updateOn: "blur"}],
+        PID_ROLL_Ki: ["", {updateOn: "blur"}],
+        PID_ROLL_Kd: ["", {updateOn: "blur"}],
+    });
+
+
     valuesSubscription: Subscription;
 
     value = {
-        DEBUG: 0,
-        BUZZER_ENABLE: 0,
-        MEMORY_CARD_ENABLED: 0,
-        DATA_RECOVERY_MODE: 0,
-        FORMAT_MEMORY: 0,
-        // corrEn: 0,
-        // corrTime: 0,
-        // corrRate: 0,
-        // launchDetect: 0,
+        GUIDING_TYPE: 0,
+        ROLL_CONTROL_ENABLED: 0,
+        ROLL_CONTROL_TYPE: 0,
+
+        SERVO_1_AXIS: 0,
+        SERVO_2_AXIS: 0,
+        SERVO_3_AXIS: 0,
+        SERVO_4_AXIS: 0,
+
+        SERVO_1_OFFSET: 0,
+        SERVO_2_OFFSET: 0,
+        SERVO_3_OFFSET: 0,
+        SERVO_4_OFFSET: 0,
+        SERVO_1_ORIENTATION: 1,
+        SERVO_2_ORIENTATION: 1,
+        SERVO_3_ORIENTATION: 1,
+        SERVO_4_ORIENTATION: 1,
+        MAX_FINS_TRAVEL: 0,
+
+        PID_PITCH_Kp: 0,
+        PID_PITCH_Ki: 0,
+        PID_PITCH_Kd: 0,
+
+        PID_YAW_Kp: 0,
+        PID_YAW_Ki: 0,
+        PID_YAW_Kd: 0,
+        
+        PID_ROLL_Kp: 0,
+        PID_ROLL_Ki: 0,
+        PID_ROLL_Kd: 0,
     };
 
     alignementX = 90;
@@ -40,17 +95,44 @@ export class GuidanceComponent implements OnInit {
         const decoded = _.split(value, '|');
 
         return {
-            DEBUG: parseInt(decoded[0], 2) == 1 ? true : false,
-            BUZZER_ENABLE: parseInt(decoded[1], 2) == 1 ? true : false,
-            MEMORY_CARD_ENABLED: parseInt(decoded[2], 2) == 1 ? true : false,
-            DATA_RECOVERY_MODE: parseInt(decoded[3], 2) == 1 ? true : false,
-            FORMAT_MEMORY: parseInt(decoded[4], 2) == 1 ? true : false,
+            GUIDING_TYPE: parseInt(decoded[0], 10),
+            ROLL_CONTROL_ENABLED: parseInt(decoded[1], 2) == 1 ? true : false,
+            ROLL_CONTROL_TYPE: parseInt(decoded[2], 10),
+    
+            SERVO_1_AXIS: decoded[3],
+            SERVO_2_AXIS: decoded[4],
+            SERVO_3_AXIS: decoded[5],
+            SERVO_4_AXIS: decoded[6],
+    
+            SERVO_1_OFFSET: parseInt(decoded[7], 10),
+            SERVO_2_OFFSET: parseInt(decoded[8], 10),
+            SERVO_3_OFFSET: parseInt(decoded[9], 10),
+            SERVO_4_OFFSET: parseInt(decoded[10], 10),
+
+            SERVO_1_ORIENTATION: parseInt(decoded[11], 10),
+            SERVO_2_ORIENTATION: parseInt(decoded[12], 10),
+            SERVO_3_ORIENTATION: parseInt(decoded[13], 10),
+            SERVO_4_ORIENTATION: parseInt(decoded[14], 10),
+            MAX_FINS_TRAVEL: parseInt(decoded[15], 10),
+    
+            PID_PITCH_Kp: parseFloat(decoded[16]),
+            PID_PITCH_Ki: parseFloat(decoded[17]),
+            PID_PITCH_Kd: parseFloat(decoded[18]),
+    
+            PID_YAW_Kp: parseFloat(decoded[19]),
+            PID_YAW_Ki: parseFloat(decoded[20]),
+            PID_YAW_Kd: parseFloat(decoded[21]),
+            
+            PID_ROLL_Kp: parseFloat(decoded[22]),
+            PID_ROLL_Ki: parseFloat(decoded[23]),
+            PID_ROLL_Kd: parseFloat(decoded[24]),
+
         };
     }
 
     updateValue(value) {
         this.value = value;
-        // this.sysPrefsForm.setValue(this.value);
+        this.guidanceForm.setValue(this.value);
     }
 
     disconnect() {
@@ -60,6 +142,9 @@ export class GuidanceComponent implements OnInit {
 
     hasError(error: string) {
     }
+
+    update(e) {}
+
 
     ngOnDestroy() {
         if (this.valuesSubscription) {
